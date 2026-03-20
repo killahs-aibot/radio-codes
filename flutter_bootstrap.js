5c0a0f8ead541c3e5ef09a935270885d066c4672
KillahMods <killahmods@github.com>
2026-03-20 17:28:35 +0000

    RadioUnlock v4: Serial auto-detect, brand grid, Kia 10x10

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -51,7 +51,7 @@ if (flet.noCdn) {
 _flutter.loader.load({
     config: flutterConfig,
     serviceWorkerSettings: {
-        serviceWorkerVersion: "3561576514" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
+        serviceWorkerVersion: "378951126" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
     },
     onEntrypointLoaded: async function (engineInitializer) {
         const engine = await engineInitializer.initializeEngine(flutterConfig);
